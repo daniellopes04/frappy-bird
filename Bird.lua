45c9a8d42f31d3d864b31ec7edeb1b96600aec11
Daniel Lopes <daniel.lopes04@gmail.com>
2021-01-30 03:07:16 -0300

    Adding flapping wings movement and commentary

--- Bird.lua
+++ Bird.lua
@@ -9,23 +9,50 @@
 
 Bird = Class{}
 
+start = love.timer.getTime()
+
 function Bird:init()
     math.randomseed(os.time())
     num = math.random(1, 3)
+
+    -- Bird color is defined randomly
     if num == 1 then
-        birdColor = "yellow"
+        self.color = "yellow"
     elseif num == 2 then
-        birdColor = "blue"
+        self.color = "blue"
     else
-        birdColor = "red"
+        self.color = "red"
     end
-        
-    self.image = love.graphics.newImage("sprites/".. birdColor .."bird-midflap.png")
+
+    -- Defines the wing movement of the bird
+    -- 0 = midflap, 1 = upflap and 2 = downflap
+    self.flap = 0
+
+    -- Bird image is set to the respective sprite
+    self.image = love.graphics.newImage("sprites/".. self.color .."bird-midflap.png")
+
     self.width = self.image:getWidth()
     self.height = self.image:getHeight()
-
     self.x = VIRTUAL_WIDTH / 2 - (self.width / 2)
     self.y = VIRTUAL_HEIGHT / 2 - (self.height / 2)
+end
+
+function Bird:update(dt)
+    -- Updates the bird's wing movement every 0.2 seconds
+    elapsedTime = love.timer.getTime() - start
+    if elapsedTime > 0.2 then
+        self.flap = (self.flap + 1) % 3
+        start = love.timer.getTime()
+    end
+
+    -- Updates the bird sprite
+    if self.flap == 0 then
+        self.image = love.graphics.newImage("sprites/".. self.color .. "bird-midflap.png")
+    elseif self.flap == 1 then
+        self.image = love.graphics.newImage("sprites/".. self.color .. "bird-upflap.png")
+    elseif self.flap == 2 then
+        self.image = love.graphics.newImage("sprites/".. self.color .. "bird-downflap.png")
+    end
 end
 
 function Bird:render()
--- main.lua
+++ main.lua
@@ -2,7 +2,7 @@
     Part of "S50's Intro to Game Development"
     Lecture 1
     
-    -- Implementation of mobile game "Flappy Bird". --
+    -- Implementation of mobile game "Flappy Bird" --
     Made by: Daniel de Sousa
     https://github.com/daniellopes04
 ]]
@@ -15,35 +15,38 @@ push = require "push"
 -- https://github.com/vrld/hump/blob/master/class.lua
 Class = require "class"
 
+-- Bird class
 require "Bird"
 
+-- Screen's real and virtual dimensions
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
-
 VIRTUAL_WIDTH = 512
 VIRTUAL_HEIGHT = 288
 
+-- Loads background sprite and sets its scroll value
 local background = love.graphics.newImage("sprites/background-day.png")
 local backgroundScroll = 0
 
+-- Loads foreground sprite and sets its scroll value
 local ground = love.graphics.newImage("sprites/ground-day.png")
 local groundScroll = 0
 
+-- Speed at which the sprites will move to the left on screen
 local BACKGROUND_SCROLL_SPEED = 30
 local GROUND_SCROLL_SPEED = 60
 
--- Original background looping point used in the lecture.
--- local BACKGROUND_LOOPING_POINT = 413
+-- Defines a looping point based on repetition of the texture chosen
 local BACKGROUND_LOOPING_POINT = 514
 
 local bird = Bird()
 
--- Runs when the game starts, only once.
+-- Runs when the game starts, only once
 function love.load()
     love.graphics.setDefaultFilter("nearest", "nearest")
     love.window.setTitle("Frappy Bird")
 
-    -- Changes the sprites based on hour of day.
+    -- Changes the background and ground sprites based on hour of day
     currentHour = tonumber(os.date("%H"))
     if currentHour >= 18 or (currentHour >= 0 and currentHour <= 6) then
         background = love.graphics.newImage("sprites/background-night.png")
@@ -52,6 +55,7 @@ function love.load()
         background = love.graphics.newImage("sprites/background-sunset.png")
     end
 
+    -- Setting up the screen
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         vsync = true,
         fullscreen = false,
@@ -59,31 +63,37 @@ function love.load()
     })
 end
 
--- Called by love2d whenever we resize the screen.
+-- Called by love2d whenever we resize the screen
 function love.resize(w, h)
     push:resize(w, h)
 end
 
--- Keyboard entry handler, called each frame.
+-- Keyboard entry handler, called each frame
 function love.keypressed(key)
     if key == "escape" then
         love.event.quit()
     end
 end
 
---Called each frame, updates the game state components.
+--Called each frame, updates the game state components
 function love.update(dt)
+    -- Scrolling the background and ground sprites
     backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
     groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
+
+    -- Moves the bird and flaps its wings
+    bird:update()
 end
 
--- Called each frame for drawing to the screen after the update or otherwise.
+-- Called each frame for drawing to the screen after the update or otherwise
 function love.draw()
     push:start()
 
+    -- Rendering background and foreground
     love.graphics.draw(background, -backgroundScroll, 0)
     love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
 
+    -- Rendering the bird
     bird:render()
 
     push:finish()
